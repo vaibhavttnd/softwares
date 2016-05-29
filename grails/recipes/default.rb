ffce7ddc5f552e5c6585bb363e8ac87da4847312
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-29 21:27:35 +0530

    path

--- grails/recipes/default.rb
+++ grails/recipes/default.rb
@@ -20,16 +20,23 @@ end
 
 
 #vaibhav
+#
+#bash 'java and grails home' do
+#  code <<-EOH
+#export GRAILS_HOME=/usr/local/grails-2.5.1
+#export JAVA_HOME=/usr/lib/jvm/java-8-oracle-amd64
+#PATH=$PATH:$GRAILS_HOME/bin:$JAVA_HOME/bin
+#export GRAILS_HOME
+#export JAVA_HOME
+#export PATH   
+#
+#    EOH
+#end
 
-bash 'java and grails home' do
-  code <<-EOH
-export GRAILS_HOME=/usr/local/grails-2.5.1
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle-amd64
-PATH=$PATH:$GRAILS_HOME/bin:$JAVA_HOME/bin
-export GRAILS_HOME
-export JAVA_HOME
-export PATH   
-
-    EOH
+ruby_block 'set-env-java-grails-home-path' do
+  block do
+    ENV['JAVA_HOME'] = '/usr/lib/jvm/java-8-oracle-amd64'
+    ENV['GRAILS_HOME'] = '/usr/local/grails-2.5.1'
+    ENV['PATH']="#{ENV['PATH']}:#{ENV['GRAILS_HOME']}/bin:#{ENV['JAVA_HOME']}/bin"
+  end
 end
-
